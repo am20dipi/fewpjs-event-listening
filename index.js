ad45dd5831672a9d1a78d4e6797b6d2a189bce6f
Adriana <am20dipi@alum.siena.edu>
2021-04-29 22:23:44 -0400

    commit tree 39e1a80584762ec10daca5debc550810a249fca5

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', function(event) {
+    alert('I was clicked!');
+    }); 
 }
+
+// The NODE does the listening; the node is stored in the input constant
+
+// The first argument is the event name to listen for
+
+// The second argument is the callback function; it will do the work when the node "hears" the event
+
